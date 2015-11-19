8ce3a57223925ae2fc6c3112bb3aa6302d7dd0f9
smellyalater <acopleman@gmail.com>
2015-11-19 15:51:04 -0500

    sweet jesus i'm done

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -20,29 +20,48 @@ $(function() {
 
 // Mad Libs Scripts
 // On form submit, store inputs in variable and scroll page to #thinking div.
-var madLibsWords = [];
+
+madLibsWords = []
 
 $('#word-input').submit(function(event) {
-  {
-    event.preventDefault();
-    var elements = $(this).find('input[type=text]');
-    for (var i = 0; i <= elements.length - 1; i++) {
+  event.preventDefault();
+  var elements = $(this).find('input[type=text]');
+  for (var i = 0; i <= elements.length - 1; i++) {
     var value = $(elements[i]).val();
     madLibsWords.push(value);
-  } 
-
-  {
-    $('body,html').animate({'scrollTop':$('#thinking').offset().top}, 1200);
   }
-
-}
 
 console.log(madLibsWords);
 
-var story = "The " + madLibsWords[0] + " went to the " + madLibsWords[1] + ' ' +  madLibsWords[2];
-  console.log(story);
-  $('.storytime').text(story);
+$('body,html').animate({'scrollTop':$('#thinking').offset().top}, 1200);
+
 });
+
+
+$('#preload-complete').on('click', function() {
+  $('#madLibsWords0').text(madLibsWords[0]);
+  $('#madLibsWords1').text(madLibsWords[1]);
+  $('#madLibsWords2').text(madLibsWords[2]);
+  $('#madLibsWords3').text(madLibsWords[3]);
+  $('#madLibsWords4').text(madLibsWords[4]);
+  $('#madLibsWords5').text(madLibsWords[5]);
+  $('#madLibsWords6').text(madLibsWords[6]);
+  $('#madLibsWords7').text(madLibsWords[7]);
+  $('#madLibsWords8').text(madLibsWords[8]);
+  $('#madLibsWords9').text(madLibsWords[9]);
+  $('#madLibsWords10').text(madLibsWords[10]);
+  $('#madLibsWords11').text(madLibsWords[11]);
+  $('#madLibsWords12').text(madLibsWords[12]);
+  $('#madLibsWords13').text(madLibsWords[13]);
+  $('#madLibsWords14').text(madLibsWords[14]);
+  $('#madLibsWords15').text(madLibsWords[15]);
+  $('#madLibsWords16').text(madLibsWords[16]);
+});
+
+
+
+
+
 
 //Reset Button Function
 $('.reset-form').on('click', function() {
@@ -65,7 +84,4 @@ $('#submit-story').on('click', function() {
     $('#preload-complete, .story-is-ready').addClass('story-loaded');
     }, 8000)
   });
-
-
-
 });
